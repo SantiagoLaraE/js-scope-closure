ab91e2864bdc77fcc9a75c40694374bcdc8470c2
SantiagoLaraE <oscarescobar773@gmail.com>
2022-06-28 08:14:19 -0500

    Global Scope y Function Scope

---
+++ src/scope/01-global.js
@@ -0,0 +1,30 @@
+//VARIABLES
+
+var a; //Declaración
+
+var b = 'b' // Declaración y asignación
+
+b = 'bb' //reasignación
+
+var a = 'aa' //redeclaracion
+
+//Global Scope
+
+var fruit = 'Apple';
+
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit();
+
+
+
+function countries(){
+    country = "Colombia";
+    console.log(country);
+}
+
+countries();
+
+console.log(country);
