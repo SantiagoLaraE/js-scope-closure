cc39cb8724682639f951b869a1a99c5830ece333
SantiagoLaraE <oscarescobar773@gmail.com>
2022-06-28 09:43:16 -0500

    Reasignación y redeclaración

---
+++ src/scope/03-re.js
@@ -0,0 +1,34 @@
+var firstName; //Declarando = undefined
+firstName = "Santiago"; //Asignando
+console.log(firstName);
+
+var lastName = "Lara"; // Declarar / Asignar
+lastName = "Escobar"; //Reasignar
+console.log(lastName);
+
+var secondName = "Oscar"; // Declarar / Asignar
+var secondName = "Santiago"; // Redeclarar / Reasignar
+console.log(secondName);
+
+//LET
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+let fruit = "Banana";
+console.log(fruit);
+
+//CONST
+
+const animal = "dog";
+animal = "cat";
+console.log(animal);
+
+
+const vehicles = [];
+vehicles.push("Toyota");
+console.log(vehicles);
+vehicles.pop();
+console.log(vehicles);
+
